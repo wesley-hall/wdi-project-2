fe8888dd0c9f4b3f05efe26d5e8ee7c161bedea4
wesley-hall <hallwesley@outlook.com>
2019-06-26 23:08:22 +0100

    console logs removed, POST req comment added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const skiddleKey = process.env.SKIDDLE_KEY
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -8,15 +12,7 @@ app.use(express.static(`${__dirname}/dist`))
 
 app.use(bodyParser.json())
 
-const skiddleKey = process.env.SKIDDLE_KEY
-console.log('key', skiddleKey)
-
-const axios = require('axios')
-
 function eventsIndex(req, res) {
-  console.log('req', req)
-  console.log('params', req.params)
-  console.log('query', req.query)
   axios.get('https://www.skiddle.com/api/v1/events/search/', {
     params: {
       latitude: 51.509865,
@@ -32,7 +28,6 @@ function eventsIndex(req, res) {
 }
 
 function eventsShow(req, res) {
-  console.log('id', req.params.id)
   axios.get(`https://www.skiddle.com/api/v1/events/${req.params.id}`, {
     params: {
       api_key: skiddleKey
@@ -40,13 +35,11 @@ function eventsShow(req, res) {
   })
     .then(response => res.status(200).json(response.data))
     .catch(err => res.status(400).json(err))
-
 }
 
-
+// POST used because needed req body
 app.post('/api/events', eventsIndex)
 app.get('/api/events/:id', eventsShow)
-
 
 app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
