98281b7fdfaf8cb2b4934987e66fab96207188af
wesley-hall <hallwesley@outlook.com>
2019-06-26 22:59:44 +0100

    added proxy for /api/

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,13 @@ module.exports = {
     open: true,
     port: 8000,
     watchContentBase: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:4000',
+        secure: false
+      }
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
